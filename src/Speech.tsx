eae690350615e8e672474797212e4a3a655f3407
existmaster <existmaster@naver.com>
2019-03-04 22:12:12 +0900

    채점 알고리즘 탑재

--- src/Speech.tsx
+++ src/Speech.tsx
@@ -1,15 +1,17 @@
-import * as React from 'react';
-import Voice from 'react-native-voice';
-import Tts from 'react-native-tts';
+import * as React from "react";
+import Voice from "react-native-voice";
+import Tts from "react-native-tts";
+import Diff from "text-diff";
 
-import { View, StyleSheet, Text } from 'react-native';
-import { Card, Button } from 'react-native-material-ui';
+import { View, StyleSheet, Text } from "react-native";
+import { Card, Button } from "react-native-material-ui";
 
 interface Props {}
 interface State {
   isRecord: boolean;
   voice: string;
   question: string;
+  total: number;
 }
 export default class App extends React.Component<Props, State> {
   constructor(props: Props) {
@@ -18,7 +20,8 @@ export default class App extends React.Component<Props, State> {
     this.state = {
       isRecord: false,
       voice: undefined,
-      question: '',
+      question: "",
+      total: 0
     };
 
     Voice.onSpeechStart = this._onSpeechStart;
@@ -29,33 +32,50 @@ export default class App extends React.Component<Props, State> {
 
   componentWillMount() {
     this.setState({
-      question: '1947년, 사해 두루마리가 발견되었을 때 고고학자들은 새 문서가 발견될 때마다 발견자에게 보상을 주기 시작했습니다.'
+      question:
+        "1947년 사해 두루마리가 발견되었을 때 고고학자들은 새 문서가 발견될 때마다 발견자에게 보상을 주기 시작했습니다"
     });
   }
 
   render() {
-    const { isRecord, voice } = this.state;
-    const buttonLabel = isRecord ? '중지' : '읽기평가';
-    const buttonIcon = isRecord ? 'mic-off' : 'mic';
+    const { isRecord, voice, total } = this.state;
+    const buttonLabel = isRecord ? "중지" : "읽기평가";
+    const buttonIcon = isRecord ? "mic-off" : "mic";
     const voiceLabel = voice
       ? voice
       : isRecord
-      ? '무엇이든 말해보세요...'
-      : 'press Start button';
-
+      ? "무엇이든 말해보세요..."
+      : "press Start button";
+    const totalLabel = Number(total.toFixed(1));
     return (
       <Card>
-        <Text style={styles.titleStyle}>[ 한국어 읽기(올바르게 발음하기) 평가 ]</Text>
-        <Text style={styles.subTtitleStyle}>1. 아래의 한국어 문장을 (필요하면 몇 번 들어본 후) 올바르게 발음하면서 큰소리로 읽으시오.</Text>
+        <Text style={styles.titleStyle}>
+          [ 한국어 읽기(올바르게 발음하기) 평가 ]
+        </Text>
+        <Text style={styles.subTtitleStyle}>
+          1. 아래의 한국어 문장을 (필요하면 몇 번 들어본 후) 올바르게 발음하면서
+          큰소리로 읽으시오.
+        </Text>
         <Text style={styles.contentStyle}>{this.state.question}</Text>
-        {/* <Text>{voiceLabel}</Text> */}
+        <Text>{voiceLabel}</Text>
+        <Text>{totalLabel}</Text>
         <View style={styles.rowContainer}>
-            <View style={styles.button}>
-              <Button primary icon="radio" onPress={this._onOriginListen} text="원본 듣기연습" />
-            </View>
-            <View style={styles.button}>
-              <Button primary icon={buttonIcon} onPress={this._onRecordVoice} text={buttonLabel} />
-            </View>
+          <View style={styles.button}>
+            <Button
+              primary
+              icon="radio"
+              onPress={this._onOriginListen}
+              text="원본 듣기연습"
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              primary
+              icon={buttonIcon}
+              onPress={this._onRecordVoice}
+              text={buttonLabel}
+            />
+          </View>
         </View>
       </Card>
     );
@@ -66,22 +86,36 @@ export default class App extends React.Component<Props, State> {
   }
 
   private _onSpeechStart = event => {
-    console.log('onSpeechStart');
+    console.log("onSpeechStart");
     this.setState({
-      voice: '',
+      voice: ""
     });
   };
   private _onSpeechEnd = event => {
-    console.log('onSpeechEnd');
+    console.log("onSpeechEnd");
+    const diff = new Diff();
+    var result = "";
+    var total = 0;
+    const textDiff = diff.main(this.state.question, this.state.voice);
+    const wordScore = 100 / this.state.question.length;
+    textDiff.forEach(element => {
+      if (element[0] === 0) {
+        result += element[1];
+        total += wordScore * element[1].length;
+      }
+    });
+    this.setState({
+      total: total
+    });
   };
   private _onSpeechResults = event => {
-    console.log('onSpeechResults');
+    console.log("onSpeechResults");
     this.setState({
-      voice: event.value[0],
+      voice: event.value[0]
     });
   };
   private _onSpeechError = event => {
-    console.log('_onSpeechError');
+    console.log("_onSpeechError");
     console.log(event.error);
   };
 
@@ -90,51 +124,51 @@ export default class App extends React.Component<Props, State> {
     if (isRecord) {
       Voice.stop();
     } else {
-      Voice.start('ko-KR');
+      Voice.start("ko-KR");
     }
     this.setState({
-      isRecord: !isRecord,
+      isRecord: !isRecord
     });
   };
 
   private _onOriginListen = () => {
     Tts.stop();
-    Tts.setDefaultLanguage('ko-KR');
+    Tts.setDefaultLanguage("ko-KR");
     Tts.getInitStatus().then(() => {
       Tts.speak(this.state.question);
     });
-    Tts.addEventListener('tts-start', (event) => console.log("start", event));
-    Tts.addEventListener('tts-finish', (event) => console.log("finish", event));
-    Tts.addEventListener('tts-cancel', (event) => console.log("cancel", event));
-  }
+    Tts.addEventListener("tts-start", event => console.log("start", event));
+    Tts.addEventListener("tts-finish", event => console.log("finish", event));
+    Tts.addEventListener("tts-cancel", event => console.log("cancel", event));
+  };
 }
 
 const styles = StyleSheet.create({
   titleStyle: {
-      fontSize: 20,
-      fontWeight: '700',
-      margin: 20,
-      marginBottom: 10,
+    fontSize: 20,
+    fontWeight: "700",
+    margin: 20,
+    marginBottom: 10
   },
   subTtitleStyle: {
-      fontSize: 14,
-      fontWeight: '700',
-      margin: 20,
-      marginTop: 0,
+    fontSize: 14,
+    fontWeight: "700",
+    margin: 20,
+    marginTop: 0
   },
   contentStyle: {
-      fontSize: 14,
-      padding: 20,
-      margin: 20,
-      marginTop: 0,
-      backgroundColor: '#e0e0e0',
+    fontSize: 14,
+    padding: 20,
+    margin: 20,
+    marginTop: 0,
+    backgroundColor: "#e0e0e0"
   },
   rowContainer: {
     margin: 8,
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center"
   },
   button: {
-      marginHorizontal: 8,
-  },
+    marginHorizontal: 8
+  }
 });
